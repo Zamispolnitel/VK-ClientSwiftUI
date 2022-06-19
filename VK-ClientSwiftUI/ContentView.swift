08c150ed2ae47fe531389d9e0c206b18e0adf191
zamis <zamipolit@yandex.ru>
2022-06-19 05:25:41 +0300

    Initial Commit

---
+++ VK-ClientSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VK-ClientSwiftUI
+//
+//  Created by Михаил Сирицын on 19.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VK-ClientSwiftUI/VK_ClientSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VK_ClientSwiftUIApp.swift
+//  VK-ClientSwiftUI
+//
+//  Created by Михаил Сирицын on 19.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct VK_ClientSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
